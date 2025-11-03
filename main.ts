815410ee1d2a917975919396ed027fa4f6f38b30
CrojasGit <crojasgo@alumnes.santjosepobrer.es>
2025-11-03 21:04:35 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+Deno.serve(async (req: Request) => {
+  const url = new URL(req.url);
+  
+  // Si el usuario pide /noticiasIA.js, devolvemos ese archivo
+  if (url.pathname === "/noticiasIA.js") {
+    return serveFile(req, "./noticiasIA.js");
+  }
+
+  // En cualquier otro caso, servimos el index.html
+  return serveFile(req, "./index.html");
 });
